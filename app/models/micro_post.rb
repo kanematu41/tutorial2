944d65571c200ebec5b65d67ff114f88caceabf8
unknown <grenson.crtis@gmail.com>
2020-03-22 18:24:34 +0900

    Finish toy app

---
+++ app/models/micro_post.rb
@@ -0,0 +1,6 @@
+class MicroPost < ApplicationRecord
+
+	belongs_to :user
+	validates :content, length: {maximum: 140},
+	                    presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+	has_many :micro_posts
+	validates :name, presence: true
+	validates :email, presence: true
+end
